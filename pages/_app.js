e47dc674bc4c6d1fa0aff482bd251afcd97723de
Mohamed Magdy <Mohamed.Magdy2@alfuttaim.com>
2023-03-07 15:03:46 +0200

    feature/learning-projects

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import '../styles/globals.css'
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
