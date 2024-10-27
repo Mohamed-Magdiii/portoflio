55ea2f14038160a3a6ff78f08d403a79f3715522
Mohamed-Magdiii <mm88mm05@yahoo.com>
2024-10-28 01:45:08 +0400

    update-resume

--- components/Layouts/TopNavComponent.js
+++ components/Layouts/TopNavComponent.js
@@ -5,14 +5,14 @@ import Intro from './../Intro/Intro';
 const TopNavComponent = ({handleClick})=>{
     const onButtonClick = () => {
         // using Java Script method to get PDF file
-        fetch('Mohamed-Magdy-Resume.pdf').then(response => {
+        fetch('Mohamed_Magdy_Full_Stack_Developer_Resume.pdf').then(response => {
             response.blob().then(blob => {
                 // Creating new object of PDF file
                 const fileURL = window.URL.createObjectURL(blob);
                 // Setting various property values
                 let alink = document.createElement('a');
                 alink.href = fileURL;
-                alink.download = 'Mohamed-Magdy-Resume.pdf';
+                alink.download = 'Mohamed_Magdy_Full_Stack_Developer_Resume.pdf';
                 alink.click();
             })
         })
